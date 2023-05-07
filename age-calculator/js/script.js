51438e40c2d70b18b0194ca726321efee7fd886f
heitorcallipo <heitorcallipo14@gmail.com>
2023-05-07 20:35:38 -0300

    commit

--- age-calculator/js/script.js
+++ age-calculator/js/script.js
@@ -2,6 +2,10 @@ const day = document.querySelector(".day-input");
 const month = document.querySelector(".month-input");
 const year = document.querySelector(".year-input");
 const btn = document.getElementById("btn-result");
+const date = new Date();
+const currentYear = date.getFullYear();
+const currentMonth = date.getMonth();
+const currentDay = date.getDate();
 
 btn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -13,4 +17,37 @@ btn.addEventListener("click", function (e) {
   console.log(dayEntered, monthEntered, yearEntered);
 
   /*year validation*/
+  if (yearEntered < 1900 || yearEntered > currentYear) {
+    alert("Please enter a valid year");
+    return;
+  }
+  /*month validation*/
+  if (monthEntered < 1 || monthEntered > 12) {
+    alert("Please enter a valid month");
+    return;
+  }
+  /*day validation*/
+  if (dayEntered < 1 || dayEntered > 31) {
+    alert("Please enter a valid day");
+    return;
+  }
+  /* if leap year*/
+  if (
+    (yearEntered % 4 == 0 && yearEntered % 100 != 0) ||
+    yearEntered % 400 == 0
+  ) {
+    if (monthEntered == 2 && dayEntered > 29) {
+      alert("Invalid Day");
+      return;
+    }
+  } else if (
+  /* if not leap year*/
+    (yearEntered % 4 != 0 && yearEntered % 100 == 0) ||
+    yearEntered % 400 != 0
+  ) {
+    if (monthEntered == 2 && dayEntered > 28) {
+      alert("Invalid Day");
+      return;
+    }
+  }
 });
